166413f8dc179eacf3b348c4568e81307cb395af
Jarmo <jarmo.seppala@apprix.fi>
2019-01-13 11:34:06 +0200

    moar code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>Author Quiz</div>
+const Hero = () => (
+  <div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1> Author Quiz </h1>
+      <p> Selekt buk </p>
+    </div>
+  </div>
+);
+
+const Book = ({title}) => {
+  return(
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+};
+
+const Turn = ({author, books}) => {
+    return(
+      <div className="row turn" style={{backgroundColor: "white"}}>
+        <div className="col-4 offset-1">
+          <img src={author.imageUrl} className="authorimage" alt="Author" />
+        </div>
+        <div className="col-6">
+          {books.map((title) => <Book title={title} key={title}/>)}
+        </div>
+      </div>
     );
-  }
+}
+
+const Continue = () => {
+    return(
+      <div> Lorem shitsum </div>
+    );
+}
+
+const Footer = () => (
+  <div id="footer" className="row">
+    <div className="col-12">
+      <p className="text-muted credit">
+        Hi mom
+      </p>
+    </div>
+  </div>
+);
+
+const App = ({turnData}) => {
+
+    return (
+      <div className="container-fluid">
+        <Hero />
+        <Turn {...turnData}/> {/* props received by Turn will be author and books */}
+        <Continue />
+        <Footer />
+      </div>
+    );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+      name: 'Mark Twain',
+      imageUrl: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MTUzNTA5OTAz/mark-twain-9512564-1-402.jpg',
+      imageSource: 'Wikimedia',
+      books: ['Huckleberry Finn', 'Donald Duck', 'Mein Kampf']
+    },
+    {
+      name: 'Elis Lönnrot',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/28/Elias_L%C3%B6nnrot_portrait.jpg',
+      imageSource: 'Wikimedia',
+      books: ['Kalevala']
+    },
+    {
+      name: 'J.K. Rowling',
+      imageUrl: 'https://static01.nyt.com/images/2012/10/14/books/review/1014BTB/1014BTB-popup.jpg',
+      imageSource: 'The New York Times',
+      books: ['Harry Potter']
+    }
+];
+
+const getTurnData = (authors) => {
+  const allBooks = authors.reduce( (p,c,i) => {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+        author.books.some((title) =>
+          title === answer))
+  }
+
+};
+
+const state = {
+    turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
